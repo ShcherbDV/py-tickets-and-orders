73fb80d7068479e7cb847c1401f527e95b401922
Dmytro Shcherbyna <shcherbdmytro@gmail.com>
2025-08-19 20:24:12 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,9 +1,10 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.constraints import UniqueConstraint
-
-import settings
+from django.conf import settings
 
 
 class Genre(models.Model):
@@ -58,7 +59,7 @@ class MovieSession(models.Model):
 
 
 class Order(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.now)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
 
--- services/order.py
+++ services/order.py
@@ -13,17 +13,18 @@ def create_order(tickets: list[dict],
                  date: str = None
                  ) -> Order:
     user = get_user_model().objects.get(username=username)
-    order = Order.objects.create(user=user)
     if date:
         date_in_datetime_format = datetime.strptime(date, "%Y-%m-%d %H:%M")
-        order.created_at = date_in_datetime_format
+        order = Order.objects.create(user=user,
+                                     created_at=date_in_datetime_format)
+    else:
+        order = Order.objects.create(user=user)
     for ticket in tickets:
         order.tickets.create(
             row=ticket["row"],
             seat=ticket["seat"],
             movie_session_id=ticket["movie_session"],
         )
-    order.save()
 
     return order
 
